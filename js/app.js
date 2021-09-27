b9c214a70f2b91a57d7a99c297061e3495e56da6
GordonMunro <82704461+GordonMunro@users.noreply.github.com>
2021-09-27 13:25:13 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 const menu = document.querySelector('.menu-icon');
-const menuDropdown = document.querySelector('#myDropdown');
-const menuClose = document.querySelector('.modal-close');
+const menuDropdown = document.querySelector('.dropdown-toggle');
+const menuClose = document.querySelector('.close-icon');
 menu.addEventListener('click', () => {
     menuDropdown.className += " show";
+    menu.classList.add("hidden");
+    menuClose.classList.add("show");   
 });
 
 menuClose.addEventListener('click', () => {
     menuDropdown.classList.remove("show");
+    menuClose.classList.remove("show");
+    menu.classList.remove("hidden");
 });
